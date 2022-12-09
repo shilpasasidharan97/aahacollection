ee6f4cfbeabdcc82b4067bdd6014b27f7b7efbcf
shilpasasidharan97 <shilpasasidharan97@gmail.com>
2022-12-09 17:14:36 +0530

    social

--- official/urls.py
+++ official/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name = 'official'
 
 urlpatterns = [
+    path('login-page', views.loginpage, name='loginpage'),
+    path('logout',views.logout_shop, name='logout'),
+
     path('', views.officialHome, name='officialhome'),
     path('newuser-registration', views.newUserRegistration, name='newuserreg'),
     path('customer-list', views.customerList, name='customerlist'),
--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
+from website.models import Shop, ShopQrcode, ShopSocialMediaLinks, User
+
 # Register your models here.
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id','email','phone')
+    search_fields=('phone',)
+admin.site.register(User,UserAdmin)
+
+
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ('id','shop_name','email','phone','password','date')
+    search_fields=('phone',)
+admin.site.register(Shop,ShopAdmin)
+
+
+class ShopQrcodeAdmin(admin.ModelAdmin):
+    list_display = ('shop','resto_url','image')
+    search_fields=('shop',)
+admin.site.register(ShopQrcode,ShopQrcodeAdmin)
+
+
+class ShopSocialMediaLinksAdmin(admin.ModelAdmin):
+    list_display = ('shop','facebook','phone_number')
+    search_fields=('shop',)
+admin.site.register(ShopSocialMediaLinks,ShopSocialMediaLinksAdmin)
