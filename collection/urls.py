81b007fbf868d5d54c38beb2195baa594639539f
shilpasasidharan97 <shilpasasidharan97@gmail.com>
2022-12-10 16:46:20 +0530

    collection template

--- collection/urls.py
+++ collection/urls.py
@@ -5,5 +5,8 @@ app_name = 'collection'
 
 urlpatterns = [
     path('',views.collectionHome,name='home'),
-    path('products',views.products,name='products'),
+    path('subcategory/',views.subCategory,name='subcategory'),
+    path('products/',views.products,name='products'),
+    path('product-details/',views.productDetails,name='productdetails'),
+    path('cart/',views.cart,name='cart'),
 ]
--- collection/views.py
+++ collection/views.py
@@ -8,5 +8,18 @@ def collectionHome(request):
     return render(request, 'collection/home.html')
 
 
+
+def subCategory(request):
+    return render(request, 'collection/subcategory.html')
+
+
 def products(request):
     return render(request, 'collection/product.html')
+
+
+def productDetails(request):
+    return render(request, 'collection/product-details.html')
+
+
+def cart(request):
+    return render(request, 'collection/cart.html')
